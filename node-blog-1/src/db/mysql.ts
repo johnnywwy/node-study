1e701482c1f84cafc5957fccb3cc7998c99fee86
wuwenyu <johnnywwy@gmail.com>
2023-12-04 15:53:28 +0800

    feat：封装 mysql

--- node-blog-1/src/db/mysql.ts
+++ node-blog-1/src/db/mysql.ts
@@ -1,4 +1,32 @@
 import mysql from 'mysql'
 
-import { MYSQL_CONF } from '../config/db'
+import MYSQL_CONF from '../config/db'
 console.log('MYSQL_CONF', MYSQL_CONF);
+
+
+// 创建连接对象
+const con = mysql.createConnection(MYSQL_CONF)
+
+// 开始连接
+con.connect()
+
+
+/**
+ * 执行数据库操作
+ * @param {string} sql - SQL语句
+ * @returns {Promise} - 返回一个Promise对象，包含执行结果
+ */
+function exec(sql: string) {
+  return new Promise((resolve, reject) => {
+    con.query(sql, (err, result) => {
+      if (err) {
+        reject(err)
+        return
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
+
+export default exec
